a2989bbcdc03d08490d110962651dcc7a4e5d7c3
nypyp <nypypbro@gmail.com>
2024-12-14 15:56:13 +0800

    modify the class define

--- csrc/yolov7/jetson/main.cpp
+++ csrc/yolov7/jetson/main.cpp
@@ -2,30 +2,24 @@
 // Created by ubuntu on 1/20/23.
 //
 #include "opencv2/opencv.hpp"
-#include "yolov8.hpp"
 #include "yolov7.hpp"
 #include <chrono>
 
 namespace fs = ghc::filesystem;
 
-// const std::vector<std::string> CLASS_NAMES = {
-//     "person",         "bicycle",    "car",           "motorcycle",    "airplane",     "bus",           "train",
-//     "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",    "parking meter", "bench",
-//     "bird",           "cat",        "dog",           "horse",         "sheep",        "cow",           "elephant",
-//     "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",     "handbag",       "tie",
-//     "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball",  "kite",          "baseball bat",
-//     "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",       "wine glass",    "cup",
-//     "fork",           "knife",      "spoon",         "bowl",          "banana",       "apple",         "sandwich",
-//     "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",        "donut",         "cake",
-//     "chair",          "couch",      "potted plant",  "bed",           "dining table", "toilet",        "tv",
-//     "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",   "microwave",     "oven",
-//     "toaster",        "sink",       "refrigerator",  "book",          "clock",        "vase",          "scissors",
-//     "teddy bear",     "hair drier", "toothbrush"};
-
 const std::vector<std::string> CLASS_NAMES = {
-    "sofa", "cabinet", "table", "chair", "closestool", "door", "bed", "washer", "refrigerator",
-    "bottle", "cup", "shoes", "tissue", "cellphone"
-};
+    "person",         "bicycle",    "car",           "motorcycle",    "airplane",     "bus",           "train",
+    "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",    "parking meter", "bench",
+    "bird",           "cat",        "dog",           "horse",         "sheep",        "cow",           "elephant",
+    "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",     "handbag",       "tie",
+    "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball",  "kite",          "baseball bat",
+    "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",       "wine glass",    "cup",
+    "fork",           "knife",      "spoon",         "bowl",          "banana",       "apple",         "sandwich",
+    "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",        "donut",         "cake",
+    "chair",          "couch",      "potted plant",  "bed",           "dining table", "toilet",        "tv",
+    "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",   "microwave",     "oven",
+    "toaster",        "sink",       "refrigerator",  "book",          "clock",        "vase",          "scissors",
+    "teddy bear",     "hair drier", "toothbrush"};
 
 const std::vector<std::vector<unsigned int>> COLORS = {
     {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
